261c4d6c4cf036f48a5c6e66207305e7fc781b0e
ComActivity <104488499+ComActivity@users.noreply.github.com>
2022-12-13 17:17:30 +1100

    Update UpdYieldCat.groovy

--- src/main/groovy/EXT230MI/UpdYieldCat.groovy
+++ src/main/groovy/EXT230MI/UpdYieldCat.groovy
@@ -270,12 +270,12 @@ public class UpdYieldCat extends ExtendM3Transaction {
       Pattern.compile("^((2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26])))0229)\$" 
         + "|^(((19|2[0-9])[0-9]{2})02(0[1-9]|1[0-9]|2[0-8]))\$"
         + "|^(((19|2[0-9])[0-9]{2})(0[13578]|10|12)(0[1-9]|[12][0-9]|3[01]))\$" 
-        + "|^(((19|2[0-9])[0-9]{2})(0[469]|11)(0[1-9]|[12][0-9]|30))\$").matcher(dateStr)
-    dateIsValid = matcher.matches()
+        + "|^(((19|2[0-9])[0-9]{2})(0[469]|11)(0[1-9]|[12][0-9]|30))\$").matcher(dateStr);
+    dateIsValid = matcher.matches();
     if (dateIsValid) {
-      dateIsValid = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyyMMdd")) != null
+      dateIsValid = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyyMMdd")) != null;
     } 
-    return dateIsValid
+    return dateIsValid;
   }
   /*
    * updateEXTCAT - Callback function to update EXTCAT table
